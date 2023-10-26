f65b66a2b769d5ccd42c1a11ab225f63129a0b2c
vkt1414 <vthiriveedhi@mgh.harvard.edu>
2023-10-26 09:43:51 -0400

    fix new line character

--- test/src/preProcessNotebooks.py
+++ test/src/preProcessNotebooks.py
@@ -10,9 +10,9 @@ def modify_notebook(file_name, project_id, query1, query2):
     for cell in nb.cells:
         if cell.cell_type == "code":
             # Replace the line if it exists
-            cell.source = cell.source.replace('my_ProjectID = "" #@param {type:"string"}', f"this project_id is injected for testing /n my_ProjectID='{project_id}'")
+            cell.source = cell.source.replace('my_ProjectID = "" #@param {type:"string"}', f"#this project_id is injected for testing\nmy_ProjectID='{project_id}'")
             # Comment out another line
-            cell.source = cell.source.replace('auth.authenticate_user()', '#while testing, the authentication is handled by using application default credentials /n #auth.authenticate_user()')
+            cell.source = cell.source.replace('auth.authenticate_user()', '#while testing, the authentication is handled by using application default credentials\n#auth.authenticate_user()')
             cell.source = cell.source.replace('REPLACE THIS TEXT WITH YOUR QUERY!', query1)
             cell.source = cell.source.replace('# write the selection criteria under this line!', query2)            
 
@@ -30,6 +30,6 @@ files = [
 for file_name in files:
 
     project_id = 'idc-external-025'
-    query1 = '#this query is injected for testing /n SELECT DISTINCT collection_tumorLocation FROM `bigquery-public-data.idc_current.dicom_all` ' if 'part2' in file_name else ''
-    query2 = '#this query is injected for testing /n MODALITY=\'MR\' AND collection_tumorLocation=\'Lung\'' if 'part2' in file_name else ''
+    query1 = '#this query is injected for testing\n SELECT DISTINCT collection_tumorLocation FROM `bigquery-public-data.idc_current.dicom_all` ' if 'part2' in file_name else ''
+    query2 = '#this query is injected for testing\n MODALITY=\'MR\' AND collection_tumorLocation=\'Lung\'' if 'part2' in file_name else ''
     modify_notebook(file_name, project_id, query1, query2)
